f15fa56db95a2b6c8dbb578396c024ce31bd77b0
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2025-01-16 06:08:11 -0800

    complete class

---
+++ Employee.cs
@@ -0,0 +1,8 @@
+public class Employee 
+{
+    public int id { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    public decimal Salary {get; set;}
+} 
---
+++ User.cs
@@ -0,0 +1,9 @@
+public class User
+{
+     public int id { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
