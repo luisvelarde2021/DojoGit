691ccab6f30c40379a2f191bd24501959dc3ff8f
Luis Enrique Velarde Navarro <luisvelarde2021@hotmail.com>
2025-01-16 06:13:39 -0800

    Commit to reverse

--- User.cs
+++ User.cs
@@ -4,6 +4,5 @@ public class User
     public string FirstName { get; set; }
 
     public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+   
 }
